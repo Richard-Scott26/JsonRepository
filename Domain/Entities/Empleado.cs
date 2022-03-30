4f86003302f6fafbffc9df048c3b59556c166b4f
Richard-Scott26 <95006989+Richard-Scott26@users.noreply.github.com>
2022-03-30 12:04:16 -0600

    Add files via upload

---
+++ Domain/Entities/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Enum;
+
+namespace Domain.Entities
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public String Cedula { get; set; }
+        public String Nombres { get; set; }
+        public String Apellidos { get; set; }
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+        public String Email { get; set; }
+        public EstadoEmpleado estadoEmpleado { get; set; }
+    }
+}
